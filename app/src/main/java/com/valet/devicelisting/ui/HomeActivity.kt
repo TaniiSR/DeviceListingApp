2a4eff1b5c836de553955310cebbc4b01c9acdf8
Tanveer Munir <>
2022-02-02 12:36:33 +0500

    Build Src added for dependencies management

--- app/src/main/java/com/valet/devicelisting/ui/HomeActivity.kt
+++ app/src/main/java/com/valet/devicelisting/ui/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.valet.devicelisting.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.valet.devicelisting.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,21 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.2"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20"
+        classpath(BuildClassesConfig.ANDROID_GRADLE_PLUGIN)
+        classpath(BuildClassesConfig.KOTLIN_GRADLE_PLUGIN)
+        classpath(BuildClassesConfig.HILT_GRADLE_PLUGIN)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
